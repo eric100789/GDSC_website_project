caf5273eb483150c192541db0fa22f08dd1b9a57
eric100789 <eric100789@yahoo.com.tw>
2022-05-04 20:27:32 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+blah.src="profile.jpg"
+imgInp.onchange = evt => {
+  const [file] = imgInp.files
+  if (file) {
+    blah.src = URL.createObjectURL(file)
+  }
+}
+document.getElementById("name").innerHTML = "Name";
